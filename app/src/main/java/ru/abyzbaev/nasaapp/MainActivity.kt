be3c3d996094de084c57c6f5c7d127dc29e17b35
Timur <timur@vlaga.pro>
2023-01-09 11:38:30 +0500

    Базовый функционал

--- app/src/main/java/ru/abyzbaev/nasaapp/MainActivity.kt
+++ app/src/main/java/ru/abyzbaev/nasaapp/MainActivity.kt
@@ -2,16 +2,20 @@ package ru.abyzbaev.nasaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.abyzbaev.nasaapp.ui.main.MainFragment
+import ru.abyzbaev.nasaapp.databinding.ActivityMainBinding
+import ru.abyzbaev.nasaapp.ui.main.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
         }
     }
