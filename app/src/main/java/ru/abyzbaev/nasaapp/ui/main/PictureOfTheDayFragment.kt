1d2f939919d65307931723073edb176a18ec1807
TimurHomePC <timstudent15@gmail.com>
2023-02-11 15:59:23 +0500

    BottomNavigationDrawer added

--- app/src/main/java/ru/abyzbaev/nasaapp/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/abyzbaev/nasaapp/ui/main/PictureOfTheDayFragment.kt
@@ -9,19 +9,20 @@ import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat.recreate
+import androidx.core.content.ContextCompat
 import coil.load
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import ru.abyzbaev.nasaapp.BottomNavigationDrawerFragment
 import ru.abyzbaev.nasaapp.MainActivity
 import ru.abyzbaev.nasaapp.PictureOfTheDayData
 import ru.abyzbaev.nasaapp.R
@@ -49,7 +50,7 @@ class PictureOfTheDayFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewModel.getData().observe(viewLifecycleOwner){ renderData(it) }
+        viewModel.getData().observe(viewLifecycleOwner) { renderData(it) }
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
@@ -57,40 +58,85 @@ class PictureOfTheDayFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.inputLayout.setEndIconOnClickListener{
+        binding.inputLayout.setEndIconOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+                data =
+                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
             })
         }
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
-        binding.buttonThm1.setOnClickListener{
-            Toast.makeText(requireContext(), "Theme Indigo apply", Toast.LENGTH_SHORT).show()
-            themeId = R.style.IndigoTheme
-            activity?.recreate()
+        setBottomAppBar(view)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                activity?.let {
+                    BottomNavigationDrawerFragment().show(it.supportFragmentManager, tag)
+                }
+            }
         }
-        binding.buttonThm2.setOnClickListener{
-            Toast.makeText(requireContext(), "Theme Pink apply", Toast.LENGTH_SHORT).show()
-            themeId = R.style.PinkTheme
-            activity?.recreate()
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBottomAppBar(view: View) {
+        val context = activity as MainActivity
+        context.setSupportActionBar(view.findViewById(R.id.bottom_app_bar))
+        setHasOptionsMenu(true)
+        binding.fab.setOnClickListener {
+            if (isMain) {
+                isMain = false
+                binding.bottomAppBar.navigationIcon = null
+                binding.bottomAppBar.fabAlignmentMode =
+                    BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.ic_back_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other_screen)
+            } else {
+                isMain = true
+                binding.bottomAppBar.navigationIcon =
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.ic_hamburger_menu_bottom_bar
+                    )
+                binding.bottomAppBar.fabAlignmentMode =
+                    BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        context,
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
         }
     }
 
 
-    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout){
+    private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
         bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
-    private fun renderData(data: PictureOfTheDayData){
-        when(data){
+    private fun renderData(data: PictureOfTheDayData) {
+        when (data) {
             is PictureOfTheDayData.Success -> {
                 val serverResponseData = data.serverResponseData
                 val url = serverResponseData.url
-                if (url.isNullOrEmpty()){
+                if (url.isNullOrEmpty()) {
                     //Отобразить ошибку
                     //showError("Сообщение, что ссылка пустая")
                     toast("Link is empty")
-                }else{
+                } else {
                     //Отобразите фото
                     showSuccess()
                     //Coil в работе: достаточно вызвать у нашего ImageView нужную
@@ -103,15 +149,17 @@ class PictureOfTheDayFragment : Fragment() {
                         placeholder(R.drawable.ic_no_photo_vector)
                         crossfade(true)
                     }
-                    requireView().findViewById<TextView>(R.id.bottomSheetDescriptionHeader).text = serverResponseData.title
-                    requireView().findViewById<TextView>(R.id.bottomSheetDescription).text = serverResponseData.explanation
+                    requireView().findViewById<TextView>(R.id.bottomSheetDescriptionHeader).text =
+                        serverResponseData.title
+                    requireView().findViewById<TextView>(R.id.bottomSheetDescription).text =
+                        serverResponseData.explanation
                 }
             }
-            is PictureOfTheDayData.Loading ->{
+            is PictureOfTheDayData.Loading -> {
                 //Отобразите загрузку
                 showLoading()
             }
-            is PictureOfTheDayData.Error ->{
+            is PictureOfTheDayData.Error -> {
                 //Отобразите ошибку
                 //showError(data.error.message)
                 toast(data.error.message)
@@ -128,7 +176,7 @@ class PictureOfTheDayFragment : Fragment() {
         this.binding.loading.loadingLayout.visibility = VISIBLE
     }
 
-    private fun toast(string: String?){
+    private fun toast(string: String?) {
         Toast.makeText(context, string, Toast.LENGTH_SHORT).apply {
             setGravity(Gravity.BOTTOM, 0, 250)
             show()
@@ -144,5 +192,6 @@ class PictureOfTheDayFragment : Fragment() {
         fun newInstance() = PictureOfTheDayFragment()
         private const val THEME = "THEME"
         private var themeId = R.style.Theme_NasaApp
+        private var isMain = true
     }
 }
