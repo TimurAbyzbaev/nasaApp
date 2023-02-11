97e72b3516ced06c29ac4dd215ebc71f1fa30fc4
Timur <timur@vlaga.pro>
2023-02-11 12:05:00 +0500

    Themes part 2. Не законченное

--- app/src/main/java/ru/abyzbaev/nasaapp/MainActivity.kt
+++ app/src/main/java/ru/abyzbaev/nasaapp/MainActivity.kt
@@ -6,14 +6,16 @@ import ru.abyzbaev.nasaapp.databinding.ActivityMainBinding
 import ru.abyzbaev.nasaapp.ui.main.PictureOfTheDayFragment
 
 class MainActivity : AppCompatActivity() {
-
+    val THEME = "THEME"
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTheme(R.style.PinkTheme)
         if (savedInstanceState == null) {
+
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PictureOfTheDayFragment.newInstance())
                 .commitNow()
--- app/src/main/java/ru/abyzbaev/nasaapp/ui/main/PictureOfTheDayFragment.kt
+++ app/src/main/java/ru/abyzbaev/nasaapp/ui/main/PictureOfTheDayFragment.kt
@@ -1,6 +1,9 @@
 package ru.abyzbaev.nasaapp.ui.main
 
+import android.Manifest
+import android.app.Application
 import android.content.Intent
+import android.content.res.Resources
 import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -16,6 +19,7 @@ import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import coil.load
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import ru.abyzbaev.nasaapp.MainActivity
 import ru.abyzbaev.nasaapp.PictureOfTheDayData
 import ru.abyzbaev.nasaapp.R
 import ru.abyzbaev.nasaapp.databinding.FragmentMainBinding
@@ -24,10 +28,17 @@ class PictureOfTheDayFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    //private lateinit var themeId: Int
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private val viewModel: PictureOfTheDayViewModel by lazy {
         ViewModelProvider.NewInstanceFactory().create(PictureOfTheDayViewModel::class.java)
+    }
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -35,6 +46,7 @@ class PictureOfTheDayFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         viewModel.getData().observe(viewLifecycleOwner){ renderData(it) }
+
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -47,7 +59,24 @@ class PictureOfTheDayFragment : Fragment() {
             })
         }
         setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+        binding.buttonThm1.setOnClickListener{
+            requireContext().setTheme(R.style.IndigoTheme)
+            //o
+            //onSaveInstanceState()
+            requireActivity().recreate()
+        }
+        binding.buttonThm2.setOnClickListener{
+            requireContext().setTheme(R.style.PinkTheme)
+            requireActivity().recreate()
+        }
     }
+
+    //saving state
+    /*override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(MainActivity().THEME, )
+    }*/
+
 
     private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout){
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
